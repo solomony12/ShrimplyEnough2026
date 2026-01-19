e24d054d58171c6b56e7288923d35b8ddc3ee0eb
Solomon Yan <u1297631@gcloud.utah.edu>
2026-01-19 03:30:27 -0700

    Tutorial video added. Players must watch tutorial before they can play the game

---
+++ Assets/Scripts/VideoEndDetector.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class VideoEndDetector : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string videoScene;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoFinished;
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        SceneManager.UnloadSceneAsync(videoScene);
+    }
+}
